9068f9bf0977420e005e9f4dd31e744087bfc088
davidbritton1 <102608670+davidbritton1@users.noreply.github.com>
2022-05-10 07:59:51 -0700

    Initial Installation error. SQL server would not run & packages would not install. Restarted everything

--- music_project/urls.py
+++ music_project/urls.py
@@ -1,4 +1,4 @@
-"""music_library_project URL Configuration
+"""music_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
